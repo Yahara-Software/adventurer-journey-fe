72945d07835605cba0cf38d06ff6acd11bd7a3c7
Josh Peot <jpeot@yaharasoftware.com>
2023-10-27 17:32:44 -0500

    base app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Your code starts here... Have fun!
+      </header>
+    </div>
+  );
+}
+
+export default App;
